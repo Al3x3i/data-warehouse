90084ac76930ecb778b524da4706ecb74eaf3f03
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-10-31 19:39:41 +0100

    Add: find sum 'Total Clicks'

--- src/main/java/com/data/warehouse/report/StatisticRepository.java
+++ src/main/java/com/data/warehouse/report/StatisticRepository.java
@@ -1,7 +1,15 @@
 package com.data.warehouse.report;
 
+import java.time.LocalDate;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StatisticRepository extends JpaRepository<Statistic, UUID> {
+
+    @Query("SELECT SUM(clicks) " +
+            "FROM Statistic " +
+            "WHERE DAILY BETWEEN :startDate AND :endDate AND DATA_SOURCE = :dataSource")
+    long findSumTotalClicks(String dataSource, LocalDate startDate, LocalDate endDate);
+
 }
--- src/test/java/com/data/warehouse/StaticRepositoryTest.java
+++ src/test/java/com/data/warehouse/StaticRepositoryTest.java
@@ -3,12 +3,14 @@ package com.data.warehouse;
 import com.data.warehouse.report.Statistic;
 import com.data.warehouse.report.StatisticRepository;
 import java.time.LocalDate;
+import java.util.stream.IntStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.BDDAssertions.then;
 
 @DataJpaTest
@@ -19,7 +21,7 @@ public class StaticRepositoryTest {
     private StatisticRepository statisticRepository;
 
     @Test
-    public void test_create_statistic_record() {
+    public void should_create_statistic_record() {
 
         // Then
         var statistic = Statistic.builder()
@@ -35,5 +37,43 @@ public class StaticRepositoryTest {
 
         // Then
         then(statisticRepository.count()).isNotZero();
+    }
+
+    @Test
+    public void should_get_total_clicks_by_data_source_date_range() {
+
+        // Then
+        givenTenGoogleAdsStatistics();
+        givenTenFacebookAdsStatistics();
+
+        // When
+        long totalClicks = statisticRepository.findSumTotalClicks("Google Ads", LocalDate.parse("2019-10-13"), LocalDate.parse("2019-10-13"));
+
+        // Then
+        then(totalClicks).isEqualTo(100);
+    }
+
+    private void givenTenGoogleAdsStatistics() {
+        var statistics = IntStream.range(0, 10).mapToObj(index ->
+                Statistic.builder()
+                        .dataSource("Google Ads")
+                        .campaign("Adventmarkt Touristik")
+                        .date(LocalDate.parse("2019-10-13"))
+                        .clicks(10)
+                        .impressions(22425)
+                        .build()).collect(toList());
+        statisticRepository.saveAll(statistics);
+    }
+
+    private void givenTenFacebookAdsStatistics() {
+        var statistics = IntStream.range(0, 10).mapToObj(index ->
+                Statistic.builder()
+                        .dataSource("Facebook Ads")
+                        .campaign("Adventmarkt Touristik")
+                        .date(LocalDate.parse("2019-10-13"))
+                        .clicks(10)
+                        .impressions(22425)
+                        .build()).collect(toList());
+        statisticRepository.saveAll(statistics);
     }
 }
