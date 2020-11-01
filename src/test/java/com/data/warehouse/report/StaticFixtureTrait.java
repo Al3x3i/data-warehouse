a16b4f623884a8b5314302345471447d226d9042
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-11-01 20:12:42 +0100

    Chore: Refactor test data

--- src/test/java/com/data/warehouse/report/StaticFixtureTrait.java
+++ src/test/java/com/data/warehouse/report/StaticFixtureTrait.java
@@ -14,38 +14,35 @@ public interface StaticFixtureTrait {
     }
 
     default void givenTenGoogleAdsStatistics(String date) {
-        var statistics = IntStream.range(0, 10).mapToObj(index ->
-                Statistic.builder()
-                        .datasource("Google Ads")
-                        .campaign("Google Touristik")
-                        .daily(LocalDate.parse(date))
-                        .clicks(10)
-                        .impressions(20)
-                        .build()).collect(toList());
-        getStatisticRepository().saveAll(statistics);
+        var statistic = Statistic.builder()
+                .datasource("Google Ads")
+                .campaign("Google Touristik")
+                .daily(LocalDate.parse(date))
+                .clicks(100)
+                .impressions(200)
+                .build();
+        getStatisticRepository().save(statistic);
     }
 
     default void givenTenFacebookAdsStatistics() {
-        var statistics = IntStream.range(0, 10).mapToObj(index ->
-                Statistic.builder()
-                        .datasource("Facebook Ads")
-                        .campaign("Facebook Touristik")
-                        .daily(LocalDate.parse("2019-10-13"))
-                        .clicks(10)
-                        .impressions(30)
-                        .build()).collect(toList());
-        getStatisticRepository().saveAll(statistics);
+        var statistic = Statistic.builder()
+                .datasource("Facebook Ads")
+                .campaign("Facebook Touristik")
+                .daily(LocalDate.parse("2019-10-13"))
+                .clicks(100)
+                .impressions(300)
+                .build();
+        getStatisticRepository().save(statistic);
     }
 
     default void givenTenTwitterAdsStatistics() {
-        var statistics = IntStream.range(0, 10).mapToObj(index ->
-                Statistic.builder()
-                        .datasource("Twitter Ads")
-                        .campaign("Twitter Touristik")
-                        .daily(LocalDate.parse("2019-10-13"))
-                        .clicks(10)
-                        .impressions(40)
-                        .build()).collect(toList());
-        getStatisticRepository().saveAll(statistics);
+        var statistic = Statistic.builder()
+                .datasource("Twitter Ads")
+                .campaign("Twitter Touristik")
+                .daily(LocalDate.parse("2019-10-13"))
+                .clicks(100)
+                .impressions(400)
+                .build();
+        getStatisticRepository().save(statistic);
     }
 }
