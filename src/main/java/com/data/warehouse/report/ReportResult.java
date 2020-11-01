05e1c80097da7dffccb8ad03472550143aba9a1d
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-11-01 16:27:29 +0100

    Chore: Ignore 'null' fields in the report result

--- src/main/java/com/data/warehouse/report/ReportResult.java
+++ src/main/java/com/data/warehouse/report/ReportResult.java
@@ -1,38 +1,43 @@
 package com.data.warehouse.report;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class ReportResult {
 
     List<StatisticResponse> statistics = new ArrayList<>();
 
     @Getter
     @Setter
-    @ToString
     public static class StatisticResponse {
 
+        @JsonInclude(Include.NON_NULL)
         private String daily;
 
+        @JsonInclude(Include.NON_NULL)
         private String campaign;
 
+        @JsonInclude(Include.NON_NULL)
         private String dataSource;
 
+        @JsonInclude(Include.NON_NULL)
         private String totalClicks;
 
+        @JsonInclude(Include.NON_NULL)
         private String ctr;
 
+        @JsonInclude(Include.NON_NULL)
         private String impressions;
     }
 }
